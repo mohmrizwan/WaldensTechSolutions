e72591f1b82800c207f60511469505af45250418
mohmrizwan <151992791+mohmrizwan@users.noreply.github.com>
2026-08-13 12:04:07 +0530

    add link

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 import about from '../assets/images/about.png'
+import { Link } from "react-router-dom";
+
 
 export const About = () => {
   return (
